0810f97be71e9ef6c59f950206a14ae4f0679131
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-11-13 19:59:42 +0000

    initial commit

---
+++ backend/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Recipe
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at')
+    search_fields = ('title', 'ingredients')
+    list_filter = ('created_at',)
